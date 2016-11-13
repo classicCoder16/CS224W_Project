69a1489db5723321a539f611c85ce6acb5dca1b6
lmkostas <lmkostas@stanford.edu>
2016-11-13 07:05:48 -0800

    Add files via upload

--- feature_extractors.py
+++ feature_extractors.py
@@ -1,4 +1,145 @@
 import snap
+import math
+
+#####################################
+######LINK PREDICTION MEASURES#######
+#####################################
+
+#returns the length of the shortest path between 2 nodes
+def get_graph_distance(G, n1, n2, directed=False):
+	return -GetShortPath(G, n1, n2, directed):
+
+#returns the number of common neighbors between two nodes
+def get_common_neighbors(G, n1, n2):
+	return snap.GetCmnNbrs(G, n1, n2)
+
+#returns the jaccard coefficient between two nodes
+#assumes the graph is undirected
+def jaccard_coefficient(G, n1, n2):
+	common_neighbors = snap.GetCmnNbrs(G, n1, n2)
+	total_neighbors = get_out_degree(G, n1) + get_out_degree(G, n2)
+	return 0.0 if total_neighbors == 0 else float(common_neighbors)/total_neighbors
+
+#returns the adamic adar score between two nodes
+def adamic_adar(G, n1, n2, directed = False):
+	n1_neighbors = snap.TIntV()
+	snap.GetNodesAtHop(G, n1, 1, n1_neighbors, directed)
+	n2_neighbors = snap.TIntV()
+	snap.GetNodesAtHop(G, n2, 1, n2_neighbors, directed)
+	total_neighbors = set(n1_neighbors) & set(n2_neighbors)
+
+	aa = 0.0
+	for n in total_neighbors:
+		aa += 1.0/math.log(get_out_degree(G, n))
+
+	return aa
+
+#returns the preferential attachment score between two nodes
+def preferential_attachment(G, n1, n2):
+	return get_out_degree(G, n1) * get_out_degree(G, n2)
+
+#returns the katz score between two nodes
+def katz_measure(G, n1, n2, beta):
+	adjacency_matrix = get_adjacency_matrix(G)
+	idenitity_matrix = np.identity(G.GetNodes())
+	katz_scores = numpy.linalg.inv(idenitity_matrix-beta * adjacency_matrix) - idenitity_matrix
+	return katz_scores[n1][n2]
+
+#generates the adjacency matrix for the graph
+def get_adjacency_matrix(G):
+	n_rows = G.GetNodes()
+	A = np.zeros(shape =(n_rows, n_rows))
+	for edge in G.Edges():
+		A[edge.GetSrcNId()][edge.GetDstNId()]  = 1
+		A[edge.GetDstNId()][edge.GetSrcNId()]  = 1
+
+def sim_rank(G, n1, n2, gamma):
+	if n1 == n2: return 1
+
+	constant = gamma/preferential_attachment(G, n1, n2)
+	n1_neighbors = snap.TIntV()
+	snap.GetNodesAtHop(G, n1, 1, n1_neighbors, directed)
+	n2_neighbors = snap.TIntV()
+	snap.GetNodesAtHop(G, n2, 1, n2_neighbors, directed)
+	result = 0
+	for a in n1_neighbors:
+		for b in n2_neighbors:
+			result += sim_rank(G, a, b)
+	return result * constant
+
+#returns the expected number of steps required for a random walk starting
+#at n1 to reach n2
+def hitting_time(G, n1, n2, num_steps):
+	adjacency_matrix = get_adjacency_matrix(G)
+
+	# Make the final state an absorbing condition
+	adjacency_matrix[n2,:] = 0
+	adjacency_matrix[n2,n2] = 1
+
+	# Make a proper Markov matrix by row normalizing
+	adjacency_matrix = (adjacency_matrix.T/adjacency_matrix.sum(axis=1)).T
+
+	B = adjacency_matrix.copy()
+	for n in xrange(num_steps):
+    	B = dot(B,A)
+    return -B[n1][n2]
+
+#returns the expected number of steps for a random walk between n1 to reach n2
+def communte_time(G, n1, n2, num_steps):
+	return hitting_time(G, n1, n2, num_steps) + hitting_time(G, n2, n1, num_steps)
+
+#returns the normalized expected number of steps required for a random walk starting
+#at n1 to reach n2
+def hitting_time_normalized(G, n1, n2, stationary_probability_n2, num_steps):
+	adjacency_matrix = get_adjacency_matrix(G)
+
+	# Make the final state an absorbing condition
+	adjacency_matrix[n2,:] = 0
+	adjacency_matrix[n2,n2] = 1
+
+	# Make a proper Markov matrix by row normalizing
+	adjacency_matrix = (adjacency_matrix.T/adjacency_matrix.sum(axis=1)).T
+
+	B = adjacency_matrix.copy()
+	for n in xrange(num_steps):
+    	B = dot(B,A)
+    return -B[n1][n2]*stationary_probability_n2
+
+#returns the normalized expected number of steps for a random walk between n1 to reach n2
+def communte_time_normalized(G, n1, n2, stationary_probability_n1, stationary_probability_n2, num_steps):
+	return hitting_time(G, n1, n2, stationary_probability_n2, num_steps) + hitting_time(G, n2, n1, stationary_probability_n1, num_steps)
+
+#returns the pagerank between n1 to reach n2
+def rooted_page_rank(G, n1, n2, num_steps, alpha):
+	return hitting_time(G, n1, n2, num_steps) * alpha
+
+#returns if nodes n1 and n2 are in the same community
+def same_community(G, n1, n2, method):
+	CmtyV = snap.TCnComV()
+	if method == "CNM":
+		modularity = snap.CommunityCNM(UGraph, CmtyV)
+	elif method == "GN"
+		modularity = snap.CommunityGirvanNewman(UGraph, CmtyV)
+
+	for cmty in CmtyV:
+		cmty_set = set(cmty)
+		if n1 in cmty and n2 in cmty: return 1
+
+	return 0
+
+#returns the number of connections between n1 and n2 neighborhoods
+def friends_measure():
+	n1_neighbors = snap.TIntV()
+	snap.GetNodesAtHop(G, n1, 1, n1_neighbors, directed)
+	n2_neighbors = snap.TIntV()
+	snap.GetNodesAtHop(G, n2, 1, n2_neighbors, directed)
+	fm = 0
+	for a in n1_neighbors:
+		for b in n2_neighbors:
+			if a == b or G.IsEdge(a, b) or G.IsEdge(b,a):
+				fm += sim_rank(G, a, b)
+	return fm
+
 
 #################################
 ######NODE DEGREE MEASURES#######
@@ -17,8 +158,6 @@ def get_out_degree(G, n):
     	if item.GetVal1() == n.GetId(): return item.GetVal2()
 
 def get_total_degree(G, n):
-
-
 
 #################################
 ####NODE CENTRALITY MEASURES#####
