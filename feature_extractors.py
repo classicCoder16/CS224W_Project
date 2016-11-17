14651915e194daabc1c05a1b5179b8800151a43d
lmkostas <lmkostas@stanford.edu>
2016-11-16 18:19:25 -0800

    updated node links

--- feature_extractors.py
+++ feature_extractors.py
@@ -7,23 +7,42 @@ import math
 
 #returns the length of the shortest path between 2 nodes
 def get_graph_distance(G, n1, n2, directed=False):
-	if G.IsEdge(n1, n2): G.DelEdge(n1, n2)
+	deleted = False
+	if G.IsEdge(n1, n2):
+		G.DelEdge(n1, n2)
+		deleted = True
 	result = -snap.GetShortPath(G, n1, n2, directed)
-	if not G.IsEdge(n1, n2): G.AddEdge(n1, n2)
+	if deleted: G.AddEdge(n1, n2)
 
 #returns the number of common neighbors between two nodes
 def get_common_neighbors(G, n1, n2):
-	return snap.GetCmnNbrs(G, n1, n2)
+	deleted = False
+	if G.IsEdge(n1, n2):
+		G.DelEdge(n1, n2)
+		deleted = True
+	result = snap.GetCmnNbrs(G, n1, n2)
+	if deleted: G.AddEdge(n1, n2)
+	return result
 
 #returns the jaccard coefficient between two nodes
 #assumes the graph is undirected
 def jaccard_coefficient(G, n1, n2):
+	deleted = False
+	if G.IsEdge(n1, n2):
+		G.DelEdge(n1, n2)
+		deleted = True
 	common_neighbors = snap.GetCmnNbrs(G, n1, n2)
 	total_neighbors = get_out_degree(G, n1) + get_out_degree(G, n2)
-	return 0.0 if total_neighbors == 0 else float(common_neighbors)/total_neighbors
+	result = 0.0 if total_neighbors == 0 else float(common_neighbors)/total_neighbors
+	if deleted: G.AddEdge(n1, n2)
+	return result
 
 #returns the adamic adar score between two nodes
 def adamic_adar(G, n1, n2, directed = False):
+	deleted = False
+	if G.IsEdge(n1, n2):
+		G.DelEdge(n1, n2)
+		deleted = True
 	n1_neighbors = snap.TIntV()
 	snap.GetNodesAtHop(G, n1, 1, n1_neighbors, directed)
 	n2_neighbors = snap.TIntV()
@@ -34,17 +53,29 @@ def adamic_adar(G, n1, n2, directed = False):
 	for n in total_neighbors:
 		aa += 1.0/math.log(get_out_degree(G, n))
 
+	if deleted: G.AddEdge(n1, n2)
 	return aa
 
 #returns the preferential attachment score between two nodes
 def preferential_attachment(G, n1, n2):
-	return get_out_degree(G, n1) * get_out_degree(G, n2)
+	deleted = False
+	if G.IsEdge(n1, n2):
+		G.DelEdge(n1, n2)
+		deleted = True
+	rseult = get_out_degree(G, n1) * get_out_degree(G, n2)
+	if deleted: G.AddEdge(n1, n2)
+	return result
 
 #returns the katz score between two nodes
-def katz_measure(G, n1, n2, beta):
+def katz_measure(G, n1, n2, beta=0.05):
+	deleted = False
+	if G.IsEdge(n1, n2):
+		G.DelEdge(n1, n2)
+		deleted = True
 	adjacency_matrix = get_adjacency_matrix(G)
 	idenitity_matrix = np.identity(G.GetNodes())
 	katz_scores = numpy.linalg.inv(idenitity_matrix-beta * adjacency_matrix) - idenitity_matrix
+	if deleted: G.AddEdge(n1, n2)
 	return katz_scores[n1][n2]
 
 #generates the adjacency matrix for the graph
@@ -54,8 +85,17 @@ def get_adjacency_matrix(G):
 	for edge in G.Edges():
 		A[edge.GetSrcNId()][edge.GetDstNId()]  = 1
 		A[edge.GetDstNId()][edge.GetSrcNId()]  = 1
+	return A
 
-def sim_rank(G, n1, n2, gamma):
+def sim_rank(G, n1, n2, gamma=0.8):
+	deleted = False
+	if G.IsEdge(n1, n2):
+		G.DelEdge(n1, n2)
+		deleted = True
+	sim_rank_wrapper(G, n1, n2, gamma)
+	if deleted: G.AddEdge(n1, n2)
+
+def sim_rank_wrapper(G, n1, n2, gamma):
 	if n1 == n2: return 1
 
 	constant = gamma/preferential_attachment(G, n1, n2)
@@ -66,12 +106,17 @@ def sim_rank(G, n1, n2, gamma):
 	result = 0
 	for a in n1_neighbors:
 		for b in n2_neighbors:
-			result += sim_rank(G, a, b)
+			result += sim_rank_wrapper(G, a, b)
 	return result * constant
 
 #returns the expected number of steps required for a random walk starting
 #at n1 to reach n2
-def hitting_time(G, n1, n2, num_steps):
+def hitting_time(G, n1, n2, num_steps = 1000):
+	deleted = False
+	if G.IsEdge(n1, n2):
+		G.DelEdge(n1, n2)
+		deleted = True
+
 	adjacency_matrix = get_adjacency_matrix(G)
 
 	# Make the final state an absorbing condition
@@ -84,15 +129,21 @@ def hitting_time(G, n1, n2, num_steps):
 	B = adjacency_matrix.copy()
 	for n in xrange(num_steps):
 		B = dot(B,A)
+	if deleted: G.AddEdge(n1, n2)
 	return -B[n1][n2]
 
 #returns the expected number of steps for a random walk between n1 to reach n2
-def commute_time(G, n1, n2, num_steps):
+def commute_time(G, n1, n2, num_steps=1000):
 	return hitting_time(G, n1, n2, num_steps) + hitting_time(G, n2, n1, num_steps)
 
 #returns the normalized expected number of steps required for a random walk starting
 #at n1 to reach n2
-def hitting_time_normalized(G, n1, n2, stationary_probability_n2, num_steps):
+def hitting_time_normalized(G, n1, n2, num_steps = 1000):
+	deleted = False
+	if G.IsEdge(n1, n2):
+		G.DelEdge(n1, n2)
+		deleted = True
+
 	adjacency_matrix = get_adjacency_matrix(G)
 
 	# Make the final state an absorbing condition
@@ -105,18 +156,24 @@ def hitting_time_normalized(G, n1, n2, stationary_probability_n2, num_steps):
 	B = adjacency_matrix.copy()
 	for n in xrange(num_steps):
 		B = dot(B,A)
-	return -B[n1][n2]*stationary_probability_n2
+	if deleted: G.AddEdge(n1, n2)
+	return -B[n1][n2]/float(sum(B[:,n2]))
 
 #returns the normalized expected number of steps for a random walk between n1 to reach n2
-def communte_time_normalized(G, n1, n2, stationary_probability_n1, stationary_probability_n2, num_steps):
+def communte_time_normalized(G, n1, n2, num_steps = 1000):
 	return hitting_time(G, n1, n2, stationary_probability_n2, num_steps) + hitting_time(G, n2, n1, stationary_probability_n1, num_steps)
 
 #returns the pagerank between n1 to reach n2
-def rooted_page_rank(G, n1, n2, num_steps, alpha):
+def rooted_page_rank(G, n1, n2, num_steps=1000, alpha = 0.2):
 	return hitting_time(G, n1, n2, num_steps) * alpha
 
 #returns if nodes n1 and n2 are in the same community
-def same_community(G, n1, n2, method):
+def same_community(G, n1, n2, method = "CNM"):
+	deleted = False
+	if G.IsEdge(n1, n2):
+		G.DelEdge(n1, n2)
+		deleted = True
+
 	CmtyV = snap.TCnComV()
 	if method == "CNM":
 		modularity = snap.CommunityCNM(UGraph, CmtyV)
@@ -125,12 +182,19 @@ def same_community(G, n1, n2, method):
 
 	for cmty in CmtyV:
 		cmty_set = set(cmty)
-		if n1 in cmty and n2 in cmty: return 1
-
+		if n1 in cmty and n2 in cmty:
+			if deleted: G.AddEdge(n1, n2)
+			return 1
+	if deleted: G.AddEdge(n1, n2)
 	return 0
 
 #returns the number of connections between n1 and n2 neighborhoods
-def friends_measure():
+def friends_measure(G, n1, n2):
+	deleted = False
+	if G.IsEdge(n1, n2):
+		G.DelEdge(n1, n2)
+		deleted = True
+
 	n1_neighbors = snap.TIntV()
 	snap.GetNodesAtHop(G, n1, 1, n1_neighbors, directed)
 	n2_neighbors = snap.TIntV()
@@ -140,6 +204,7 @@ def friends_measure():
 		for b in n2_neighbors:
 			if a == b or G.IsEdge(a, b) or G.IsEdge(b,a):
 				fm += sim_rank(G, a, b)
+	if deleted: G.AddEdge(n1, n2)
 	return fm
 
 
