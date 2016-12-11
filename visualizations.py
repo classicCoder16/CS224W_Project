7292e37707603bb8d4cb8a92d6f7f0d3f669ab23
lmkostas <lmkostas@stanford.edu>
2016-12-10 21:16:15 -0800

    Add files via upload

--- visualizations.py
+++ visualizations.py
@@ -1,6 +1,6 @@
 import numpy
 from sklearn.ensemble import RandomForestClassifier
-#from weight_evolution import EvolModel
+from weight_evolution import EvolModel
 import pylab
 import collections
 import math
@@ -10,14 +10,13 @@ def shortest_paths_misclassified(train_features_file, train_example_file, test_f
 	rf = RandomForestClassifier(n_estimators=100)
 
 	train_examples = numpy.load(train_features_file)
+
 	train_zip = numpy.load(train_example_file)
 	train_labels = [tup[1] for tup in train_zip]
-	#train_pairs = [tup[0] for tup in train_zip]
 
 	test_examples = numpy.load(test_features_file)
 	test_zip = numpy.load(test_example_file)
 	test_labels = [tup[1] for tup in test_zip]
-	#test_pairs = [tup[0] for tup in test_zip]
 
 	print 'Training model', rf
 	rf.fit(train_examples, train_labels)
@@ -25,35 +24,79 @@ def shortest_paths_misclassified(train_features_file, train_example_file, test_f
 
 	print "Getting Shortest Paths"
 	short_paths = []
-	for i in range(preds.size):
-		if preds[i] != test_labels[i]:
-			short_paths.append(abs(train_examples[i][0]))
+	short_paths_false_pos = []
+	short_paths_false_neg = []
+	for i in range(preds.size):			
+		if preds[i] == -1 and test_labels[i] == 1:
+			dist = abs(train_examples[i][0])
+			if dist == 1: dist = 11.0
+			short_paths.append(dist)
+			short_paths_false_neg.append(dist)
+		elif preds[i] == 1 and test_labels[i] == -1:
+			dist = abs(train_examples[i][0])
+			if dist == 1: dist = 11.0
+			short_paths.append(dist)
+			short_paths_false_pos.append(dist)
 
-	pylab.figure()
-	pylab.hist(short_paths, bins=numpy.arange(11) - 0.5)
-	pylab.yscale('log')
-	pylab.xlabel('Shortest Path')
-	pylab.ylabel('Number of Edges')
-	pylab.title('Shortest Paths Between Misclassified ' +etype+ ' Edges')
-	pylab.show()
-
-	pylab.figure()
 	counts = collections.Counter(short_paths)
-	print counts
+	counts_neg = collections.Counter(short_paths_false_neg)
+	counts_pos = collections.Counter(short_paths_false_pos)
+	print counts, counts_neg, counts_pos
 	x = []
 	y = []
-	area = []
-	for c in counts:
-		x.append(c)
-		y.append(counts[c]) 
-		area.append(100*math.sqrt(counts[c]))
-	pylab.scatter(x, y, s=area, c=numpy.random.rand(len(x)), alpha=.5)
-	pylab.yscale('log')
-	pylab.xlabel('Shortest Path')
-	pylab.ylabel('Number of Edges')
-	pylab.title('Shortest Paths Between Misclassified ' +etype+ ' Edges')
-	pylab.show()
+	y_neg = []
+	y_pos = []
+	for i in range(1, 12):
+		x.append(i)
+		if i in counts: 
+			y.append(counts[float(i)])
+		else:
+			y.append(0) 
+		if i in counts_neg:
+			y_neg.append(counts_neg[float(i)]) 
+		else:
+			y_neg.append(0)
+		if i in counts_pos:
+			y_pos.append(counts_pos[float(i)])
+		else:
+			y_pos.append(0)
 
+	labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'No Path']
+
+	f = pylab.figure()
+	ax = f.add_axes([0.1, 0.1, 0.8, 0.8])
+	ax.bar(x, y, align='center')
+	ax.set_yscale('log')
+	ax.set_xticks(x)
+	ax.set_xticklabels(labels)
+	ax.set_xlabel('Shortest Path')
+	ax.set_ylabel('Number of Edges')
+	ax.set_title('Shortest Paths Between Misclassified ' +etype+ ' Edges')
+	f.show()
+	
+	
+	f = pylab.figure()
+	ax = f.add_axes([0.1, 0.1, 0.8, 0.8])
+	ax.bar(x, y_neg, align='center')
+	ax.set_yscale('log')
+	ax.set_xticks(x)
+	ax.set_xticklabels(labels)
+	ax.set_xlabel('Shortest Path')
+	ax.set_ylabel('Number of Edges')
+	ax.set_title('Shortest Paths Between False Negative ' +etype+ ' Edges')
+	f.show()
+
+	f = pylab.figure()
+	ax = f.add_axes([0.1, 0.1, 0.8, 0.8])
+	ax.bar(x, y_pos, align='center')
+	ax.set_yscale('log')
+	ax.set_xticks(x)
+	ax.set_xticklabels(labels)
+	ax.set_xlabel('Shortest Path')
+	ax.set_ylabel('Number of Edges')
+	ax.set_title('Shortest Paths Between False Positive ' +etype+ ' Edges')
+	f.show()
+	
 shortest_paths_misclassified("train_four_pin_features.npy", "train_four_pin_examples.npy", "test_four_pin_features.npy", "test_four_pin_examples.npy", "Pinned")
 shortest_paths_misclassified("train_four_fol_features.npy", "train_four_fol_examples.npy", "test_four_fol_features.npy", "test_four_fol_examples.npy", "Follows")
 
@@ -62,12 +105,9 @@ def false_negative_timestamps(train_features_file, train_example_file, test_feat
 
 	attr = numpy.load(attribute_file).item()
 
-
-
 	train_examples = numpy.load(train_features_file)
 	train_zip = numpy.load(train_example_file)
 	train_labels = [tup[1] for tup in train_zip]
-	#train_pairs = [tup[0] for tup in train_zip]
 
 	test_examples = numpy.load(test_features_file)
 	test_zip = numpy.load(test_example_file)
@@ -105,14 +145,14 @@ def false_negative_timestamps(train_features_file, train_example_file, test_feat
 
 
 false_negative_timestamps("train_temp_pin_features.npy", "train_temp_pin_examples.npy", "test_temp_pin_features.npy", "test_temp_pin_examples.npy", "smallest_test_attr.npy", "Pinned")
+false_negative_timestamps("train_temp_fol_features.npy", "train_temp_fol_examples.npy", "test_temp_fol_features.npy", "test_temp_fol_examples.npy", "smallest_test_attr.npy", "Follows")
 
 def evolution_heat_map(train_features_file, train_example_file, test_features_file, test_example_file, etype):
-	bliss_model = EvolModel()
+	bliss = EvolModel()
 
 	train_examples = numpy.load(train_features_file)
 	train_zip = numpy.load(train_example_file)
 	train_labels = [tup[1] for tup in train_zip]
-	#train_pairs = [tup[0] for tup in train_zip]
 
 	test_examples = numpy.load(test_features_file)
 	test_zip = numpy.load(test_example_file)
@@ -121,6 +161,21 @@ def evolution_heat_map(train_features_file, train_example_file, test_features_fi
 	print 'Training model', bliss
 	bliss.fit(train_examples, train_labels)
 	w = bliss.w
+
+	mat_len = w.size
+	w = numpy.append(w, w).reshape(2, mat_len)
+
+	print w
+
+	pylab.imshow(w, cmap=pylab.get_cmap("Blues"))
+	pylab.title('Bliss\'s Evolutionary Algorithm' +etype+ ' Feature Weights')
+	pylab.xlabel('Features (increasing with time)')
+	pylab.show()
+
+evolution_heat_map("train_temp_pin_features.npy", "train_temp_pin_examples.npy", "test_temp_pin_features.npy", "test_temp_pin_examples.npy", "Pinned")
+evolution_heat_map("train_temp_fol_features.npy", "train_temp_fol_examples.npy", "test_temp_fol_features.npy", "test_temp_fol_examples.npy", "Follows")
+
+
 
 
 
